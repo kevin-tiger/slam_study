558027ab9edfb6b50e47652ff46779c54140476a
Zhao Hongfeng <hongfeng.zhao@semidrive.com>
2023-06-09 15:35:37 +0800

    update

--- app/run_eskf_imu_gps.cc
+++ app/run_eskf_imu_gps.cc
@@ -6,9 +6,23 @@ string txt_path = "./data/ch3/10.txt";
 int main(int argc, char** argv)
 {
     cout << "app start" << endl;
-
-
-
+    TxtIO io(txt_path);
+    io.SetIMUProcessFunc([&](const IMU& imu) 
+    {
+        // cout << fixed << setprecision(3) << "imu_" << imu.timestamp_ << endl;
+    }
+    );
+    io.SetGNSSProcessFunc([&](const GNSS& gnss)
+    {
+        // cout << fixed << setprecision(3) << "gnss_" << gnss.unix_time_ << endl;
+    }
+    );
+    io.SetOdomProcessFunc([&](const Odom& odom) 
+    {
+        // cout << fixed << setprecision(3) << "odom_" << odom.timestamp_ << endl;
+    }
+    );
+    io.Go();
     cout << "app end" << endl;
     while(1);
     return 0;
