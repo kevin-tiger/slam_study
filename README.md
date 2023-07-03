0c51141588556eb9d951a096c8f788d2a76c09c4
Zhao Hongfeng <hongfeng.zhao@semidrive.com>
2023-07-03 09:10:17 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # reference
 https://github.com/gaoxiang12/slam_in_autonomous_driving
+
+
+![image](https://github.com/kevin-tiger/slam_study/blob/master/doc/lio_demo.gif )   
--- app/run_localization.cc
+++ app/run_localization.cc
@@ -1,10 +1,18 @@
 #include "common/basetype.h"
+#include "fusion/fusion.h"
 
 string config_yaml = "./config/mapping.yaml";
 
 int main(int argc, char** argv)
 {
     cout << "app start" << endl;
+
+    // sad::Fusion fusion(FLAGS_config_yaml);
+    // if (!fusion.Init()) {
+    //     return -1;
+    // }
+    Fusion fusion(config_yaml);
+    fusion.Init();
 
     cout << "app end" << endl;
     while(1);
